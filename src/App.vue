202cea166d211f80dadc9bf2a3053d97e1e98b21
Doston <dostonsayfulloyev>
2021-12-04 15:36:05 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid autem ducimus eaque earum eos, ex fugit harum iste itaque libero molestiae reiciendis repellat reprehenderit totam ullam vero. Nemo, similique.
+  </h1>
+</template>
